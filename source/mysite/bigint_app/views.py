39175972fe3deb767443fdafc18d65c1b13b4408
Jake Mathai <jakemathai5@gmail.com>
2019-08-21 16:01:32 -0700

    Update views.py

--- source/mysite/bigint_app/views.py
+++ source/mysite/bigint_app/views.py
@@ -1,24 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.utils import timezone
+from django.template import loader
 from django.core.exceptions import ObjectDoesNotExist
 from bigint_app.models import div, div_rule
 import random
+import string
 import os
 import subprocess
 # Create your views here.
-
+#dir = '/home/ec2-user/desktop/bigint_django/source/mysite/bigint_app/bigint_code'
 dir = '/Users/Jacob/desktop/programming/python/programs/bigint_django/source/mysite/bigint_app/bigint_code'
+a
+def randstr():
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(6))
 
 def index(request):
-	return HttpResponse("YOU ARE AT THE INDEX OF BIGINT_APP")
+    context = { 'div_rules': div_rule.objects.filter(rule_size__lt=300) }
+    return HttpResponse(loader.get_template('bigint_app/index.html').render(context,request))
 
 # Generate random operands and execute div operation
 def div_random(request, dividend_size, divisor_size):
     if dividend_size.isdigit() == 0 or divisor_size.isdigit() == 0 or int(dividend_size) <= 0 or int(divisor_size) <= 0:
         return HttpResponse('Invalid input')
     os.chdir(dir)
-    operands_file = 'ops_rand' + str(random.randint(1,100000)) + '.txt' # Write operands to file
+    operands_file = randstr() + '.txt' # Write operands to file
     os.system('./generate_operands ' + dividend_size + ' ' + divisor_size + ' > ' + operands_file)
     operands = open(operands_file).readlines()[0].split(',') # Parse operands in file
     r = get_or_create_rule(operands[0], operands_file) # Find or create div rule for divisor operand
@@ -35,15 +42,19 @@ def div_random(request, dividend_size, divisor_size):
     }
     return JsonResponse(data)
 
+def div_random_ui(request, dividend_size, divisor_size):
+    i = div_random(request, dividend_size, divisor_size)
+    return HttpResponse(i.content)
+
 # Div operation on custom operands
-def div_custom(request, dividend, divisor):
-    if dividend.isdigit() == 0 or divisor.isdigit() == 0 or (int(divisor[-1]) % 2 == 0 and divisor is not '2') or (divisor[-1] is '5' and len(divisor) > 1):
+def div_custom(request, dividend_, divisor_):
+    if dividend_.isdigit() == 0 or divisor_.isdigit() == 0 or (int(divisor_[-1]) % 2 == 0 and divisor_ is not '2') or (divisor_[-1] is '5' and divisor_ is not '5'):
         return HttpResponse('Invalid input')
     os.chdir(dir)
-    operands_file = 'ops_cust' + str(random.randint(1,100000)) + '.txt'
-    open(operands_file, 'w').write(divisor + ',' + dividend)
-    r = get_or_create_rule(divisor, operands_file) # Find or create div rule for divisor
-    test = get_or_create_div(dividend, divisor, operands_file) # Find or create div result for operands
+    operands_file = randstr() + '.txt'
+    open(operands_file, 'w').write(divisor_ + ',' + dividend_)
+    r = get_or_create_rule(divisor_, operands_file_) # Find or create div rule for divisor
+    test = get_or_create_div(dividend_, divisor_, operands_file) # Find or create div result for operands
     os.system('rm ' + operands_file)
     data = { # Return JSON formatted div results
         'date' : test.date,
@@ -61,7 +72,7 @@ def rule_random(request, divisor_size):
     if divisor_size.isdigit() == 0 or int(divisor_size) <= 0:
         return HttpResponse('Invalid input')
     os.chdir(dir)
-    divisor_file = 'rule_rand' + str(random.randint(1,100000)) + '.txt' # Write divisor to file
+    divisor_file = randstr() + '.txt' # Write divisor to file
     os.system('./generate_operand ' + divisor_size + ' > ' + divisor_file)
     divisor_ = open(divisor_file).readlines()[0]
     test = get_or_create_rule(divisor_, divisor_file) # Generate rule for operand
@@ -78,10 +89,10 @@ def rule_random(request, divisor_size):
 
 # Generate rule for a custom operand
 def rule_custom(request, divisor_):
-    if divisor_.isdigit() == 0 or int(divisor_) <= 0 or int(divisor_[-1]) % 2 == 0 or divisor_[-1] == '5' or divisor_ == '3' or divisor_ == '1':
+    if divisor_.isdigit() == 0 or int(divisor_) <= 0 or int(divisor_[-1]) % 2 == 0 or divisor_[-1] == '5' or divisor_ == '3' or divisor_ in ['1','2','3','5']:
         return HttpResponse('Invalid input')
     os.chdir(dir)
-    divisor_file = 'rule_cust' + str(random.randint(1,100000)) + '.txt' # Write divisor to file
+    divisor_file = randstr() + '.txt' # Write divisor to file
     open(divisor_file, 'w').write(divisor_)
     rule_ = get_or_create_rule(divisor_, divisor_file) # Compute rule
     os.system('rm ' + divisor_file)
@@ -118,7 +129,7 @@ def get_or_create_div(dividend_, divisor_, operands_file):
 
 # Check if divisor_ present in div_rule table, else compute rule and insert
 def get_or_create_rule(divisor_, operands_file):
-    if divisor_ is not '2' and divisor_ is not '3' and divisor_ is not '5':
+    if divisor_ not in ['1','2','3','5']: # No div rule for 1, 2, 3 or 5
         try: # Check if exists
             rule_ = div_rule.objects.get(divisor=divisor_)
         except ObjectDoesNotExist: # If does not exist
