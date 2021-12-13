f00dc58ae26744dfe348f7290e4d04cc87df5287
Jacob Mathai <jakemathai5@gmail.com>
2021-12-12 18:46:34 -0800

    main

--- src/api/main.py
+++ src/api/main.py
@@ -14,17 +14,17 @@ app = FastAPI()
 
 @app.get('/generate/{size}')
 def generate(size):
-    response = {'response': ''}
+    response = {'result': ''}
     try:
         size = int(size)
         if size <= 0 or size > 1e8:
             raise Exception('Invalid size specified - must be positive but not ridiculously large')
         start = datetime.now()
-        response['response'] = random_num(size)
+        response['result'] = random_num(size)
         end = datetime.now()
         response['runtime_ms'] = (end - start).microseconds / 1000
-    except:
-        response['response'] = traceback.format_exc()
+    except Exception as e:
+        response['result'] = f'failed: {e}'
     return response
 
 
@@ -38,8 +38,8 @@ def div(dividend, divisor):
         response['result'] = divisor.divides(dividend)
         end = datetime.now()
         response['runtime_ms'] = (end - start).microseconds / 1000
-    except:
-        response['result'] = f'Error: {traceback.format_exc()}'
+    except Exception as e:
+        response['result'] = f'failed: {e}'
     return response
 
 
@@ -71,55 +71,67 @@ def rand_div(dividend_size, divisor_size):
 
 @app.websocket('/ws/generate')
 async def ws_generate(websocket: WebSocket):
+    response = {'result': ''}
     await websocket.accept()
     while True:
-        data = await websocket.receive_json()
-        response = {'response': ''}
-        size = int(data['size'])
-        if size <= 0 or size > 1e8:
-            raise Exception('Invalid size specified - must be positive but not ridiculously large')
-        start = datetime.now()
-        response['response'] = random_num(size)
-        end = datetime.now()
-        response['runtime_ms'] = (end - start).microseconds / 1000
+        try:
+            data = await websocket.receive_json()
+            size = int(data['size'])
+            if size <= 0 or size > 1e8:
+                raise Exception('Invalid size specified - must be positive but not ridiculously large')
+            start = datetime.now()
+            response['result'] = random_num(size)
+            end = datetime.now()
+            response['runtime_ms'] = (end - start).microseconds / 1000
+            await websocket.send_json(response)
+        except Exception as e:
+            response['result'] = f'failed: {e}'
         await websocket.send_json(response)
 
 
 @app.websocket('/ws/div')
 async def ws_div(websocket: WebSocket):
+    response = {'result': ''}
     await websocket.accept()
     while True:
-        data = await websocket.receive_json()
-        response = {'response': ''}
-        dividend = Bigint(data['dividend'])
-        divisor = Bigint(data['divisor'])
-        start = datetime.now()
-        response['result'] = divisor.divides(dividend)
-        end = datetime.now()
-        response['runtime_ms'] = (end - start).microseconds / 1000
+        try:
+            data = await websocket.receive_json()
+            dividend = Bigint(data['dividend'])
+            divisor = Bigint(data['divisor'])
+            start = datetime.now()
+            response['result'] = divisor.divides(dividend)
+            end = datetime.now()
+            response['runtime_ms'] = (end - start).microseconds / 1000
+            await websocket.send_json(response)
+        except Exception as e:
+            response['result'] = f'failed: {e}'
         await websocket.send_json(response)
 
 
 @app.websocket('/ws/rand-div')
 async def ws_rand_div(websocket: WebSocket):
+    response = {'result': ''}
     await websocket.accept()
     while True:
-        data = await websocket.receive_json()
-        response = {'response': ''}
-        dividend_size = int(data['dividend_size'])
-        divisor_size = int(data['divisor_size'])
-        if dividend_size < divisor_size:
-            raise Exception('Dividend size must be >= divisor size')
-        if dividend_size > 1e8 or divisor_size > 1e8:
-            raise Exception('Ridiculous size requested, no thanks')
-        dividend = Bigint(dividend_size, True)
-        divisor = Bigint(divisor_size, True, True)
-        response['dividend'] = dividend.as_str()
-        response['divisor'] = divisor.as_str()
-        response['dividend_size'] = dividend.size()
-        response['divisor_size'] = divisor.size()
-        start = datetime.now()
-        response['result'] = divisor.divides(dividend)
-        end = datetime.now()
-        response['runtime_ms'] = (end - start).microseconds / 1000
+        try:
+            data = await websocket.receive_json()
+            dividend_size = int(data['dividend_size'])
+            divisor_size = int(data['divisor_size'])
+            if dividend_size < divisor_size:
+                raise Exception('Dividend size must be >= divisor size')
+            if dividend_size > 1e8 or divisor_size > 1e8:
+                raise Exception('Ridiculous size requested, no thanks')
+            dividend = Bigint(dividend_size, True)
+            divisor = Bigint(divisor_size, True, True)
+            response['dividend'] = dividend.as_str()
+            response['divisor'] = divisor.as_str()
+            response['dividend_size'] = dividend.size()
+            response['divisor_size'] = divisor.size()
+            start = datetime.now()
+            response['result'] = divisor.divides(dividend)
+            end = datetime.now()
+            response['runtime_ms'] = (end - start).microseconds / 1000
+            await websocket.send_json(response)
+        except Exception as e:
+            response['result'] = f'failed: {e}'
         await websocket.send_json(response)
